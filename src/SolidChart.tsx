8c6590df3e8521f6aeb247485df8e3dca0d2c54f
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2020-08-29 01:06:31 +0500

    add example

--- src/SolidChart.tsx
+++ src/SolidChart.tsx
@@ -1,20 +1,36 @@
 import { Chart } from "chart.js";
-import { createDependentEffect } from "solid-js";
-import { ComponentProps } from "./helpers";
-type ChartJsArgs = ConstructorParameters<typeof Chart>;
+import { Component, createEffect, createSignal } from "solid-js";
+import merge from "lodash.merge";
 export type SolidChartProps = {
-  options?: ChartJsArgs[1];
   canvasOptions?: ComponentProps<"canvas">;
-};
+} & Chart.ChartConfiguration;
+export type ComponentProps<
+  T extends keyof JSX.IntrinsicElements | Component<any>
+> = T extends Component<infer P>
+  ? P
+  : T extends keyof JSX.IntrinsicElements
+  ? JSX.IntrinsicElements[T]
+  : {};
+
 let chart: Chart;
+const deepClone = (config: SolidChartProps): SolidChartProps =>
+  JSON.parse(JSON.stringify(config));
+
 export function SolidChart(props: SolidChartProps) {
-  createDependentEffect(() => {
-    chart?.update();
-  }, [() => props]);
+  const [canvas, setCanvas] = createSignal<HTMLCanvasElement | null>(null);
+  createEffect(() => {
+    const el = canvas();
+    if (!el) return;
+    if (!chart) {
+      chart = new Chart(el, deepClone(props));
+      return;
+    }
+    merge(chart.config, deepClone(props));
+    console.log("updating chart");
+    chart.update();
+  });
   function createChart(canvas: HTMLCanvasElement) {
-    setTimeout(() => {
-      chart = new Chart(canvas, { ...props.options });
-    });
+    setTimeout(() => setCanvas(canvas));
   }
   return <canvas {...props.canvasOptions} ref={createChart} />;
 }
---
+++ src/example/Example.tsx
@@ -0,0 +1,58 @@
+import { afterEffects, createState } from "solid-js";
+import SolidChart, { SolidChartProps } from "../index";
+const edges = 6;
+const labels = ["Solid", "Is", "Most", "Performant", "JavaScript", "Framework"];
+const datasets = new Array<number[]>(edges).fill([]).map((_, i) => {
+  const arr = [0, 0, 0, 0, 0, 0];
+  if (i === edges - 1) {
+    arr[i] = 60;
+    arr[0] = 60;
+  } else {
+    arr[i] = 60;
+    arr[i + 1] = 60;
+  }
+  return {
+    backgroundColor: i % 2 === 0 ? "#3164A3" : "white",
+    data: arr,
+    label: labels[i],
+  };
+});
+export default function Stats() {
+  const settings: SolidChartProps = {
+    type: "radar",
+    data: {
+      labels,
+      datasets,
+    },
+    options: {
+      title: {
+        display: true,
+        text: "SOLID GRAPH",
+      },
+      scale: {
+        ticks: {
+          maxTicksLimit: edges,
+        },
+      },
+    },
+  };
+  const [chart, setChart] = createState(settings);
+  afterEffects(() => {
+    setInterval(() => {
+      labels.push(labels.shift());
+
+      setChart("data", "labels", [...labels]);
+
+      datasets.push(datasets.shift());
+    }, 1500);
+  });
+  return (
+    <SolidChart
+      {...chart}
+      canvasOptions={{
+        width: 1000,
+        height: 1000,
+      }}
+    />
+  );
+}
