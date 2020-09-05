4c11a4577d10b9518439eb2cd18475b40f4aacfd
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2020-09-05 16:02:02 +0500

    make chart object local & update solid

--- src/SolidChart.tsx
+++ src/SolidChart.tsx
@@ -1,33 +1,27 @@
 import { Chart } from "chart.js";
-import { Component, createEffect, createSignal } from "solid-js";
+import { ComponentProps, createEffect, createSignal } from "solid-js";
 import merge from "lodash.merge";
 export type SolidChartProps = {
   canvasOptions?: ComponentProps<"canvas">;
 } & Chart.ChartConfiguration;
-export type ComponentProps<
-  T extends keyof JSX.IntrinsicElements | Component<any>
-> = T extends Component<infer P>
-  ? P
-  : T extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[T]
-  : {};
 
-let chart: Chart;
 const deepClone = (config: SolidChartProps): SolidChartProps =>
   JSON.parse(JSON.stringify(config));
 
 export function SolidChart(props: SolidChartProps) {
   const [canvas, setCanvas] = createSignal<HTMLCanvasElement | null>(null);
+  const [chart, setChart] = createSignal<Chart | null>(null);
   createEffect(() => {
     const el = canvas();
     if (!el) return;
-    if (!chart) {
-      chart = new Chart(el, deepClone(props));
+    const _chart = chart();
+    if (!_chart) {
+      setChart(new Chart(el, deepClone(props)));
       return;
     }
-    merge(chart.config, deepClone(props));
+    merge(_chart.config, deepClone(props));
     console.log("updating chart");
-    chart.update();
+    _chart.update();
   });
   function createChart(canvas: HTMLCanvasElement) {
     setTimeout(() => setCanvas(canvas));
--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "solid-js";
-
-export type ComponentProps<
-  T extends keyof JSX.IntrinsicElements | Component<any>
-> = T extends Component<infer P>
-  ? P
-  : T extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[T]
-  : {};
